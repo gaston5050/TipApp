5e2e77f451d4f9df1f5813574136d2cc3a9d20d9
gaston5050 <80988908+gaston5050@users.noreply.github.com>
2025-10-20 16:48:30 -0300

    Proximo paso: agregar un interruptor

--- app/src/main/java/com/example/tipapp/MainActivity.kt
+++ app/src/main/java/com/example/tipapp/MainActivity.kt
@@ -5,9 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -18,9 +20,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tipapp.ui.theme.TipAppTheme
+import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +36,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TipAppTheme {
 
-                        calcularPropina()
+                        tipApp()
 
 
                 }
@@ -37,7 +44,103 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+//Desarrollo del ejercico con dos funciones
+@Composable
+fun tipApp(){
+    calcularPropina2()
+}
 
+
+//Funcion padre
+@Composable
+fun calcularPropina2(){
+
+    var montoIngresado by remember{ mutableStateOf("")}
+    var monto: Double = montoIngresado.toDoubleOrNull() ?: 0.0
+
+    var porcentajeIngresado by remember{ mutableStateOf("")}
+
+    var porcentaje: Double = porcentajeIngresado.toDoubleOrNull() ?: 0.0
+
+
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center),
+        horizontalAlignment = Alignment.CenterHorizontally){
+
+        Text(text= stringResource(R.string.calculate_tip), fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.padding(16.dp))
+
+        Text(text= "Subtotal")
+        ingresoDatos(montoIngresado, {montoIngresado = it})
+
+        Text(text= stringResource(R.string.porcentaje_de_propina))
+
+        IngresoPorcentaje(porcentajeIngresado, {porcentajeIngresado = it})
+
+
+        Spacer(modifier = Modifier.padding(16.dp))
+        if(monto != 0.0 && porcentaje != 0.0) {
+            Text(text = "Total", fontWeight = FontWeight.Bold)
+            Text(
+                text = "%.2f".format(calculoPropina(monto, porcentaje)),
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+
+    }
+
+
+
+}
+
+fun calculoPropina(monto: Double, porcentaje: Double): Double{
+
+    return monto + (monto * porcentaje)/100
+
+}
+
+
+@Composable
+fun ingresoDatos(
+    montoIngresado: String,
+    cambioMonto: (String) -> Unit,
+    modifier: Modifier = Modifier
+){
+
+    TextField(
+        value = montoIngresado,
+        onValueChange = cambioMonto,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Next),
+        modifier = modifier
+    )
+
+
+
+}
+@Composable
+fun IngresoPorcentaje(porcentajeIngresado: String,
+                      cambioPorcentaje: (String) -> Unit,
+                      modifier: Modifier = Modifier){
+
+    TextField(
+        value = porcentajeIngresado,
+        onValueChange = cambioPorcentaje,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number,imeAction = ImeAction.Done),
+        modifier = modifier
+
+    )
+}
+
+
+
+
+
+
+//Aca desarrolle todo el ejercicio en una sola funcion
+/*
 @Composable
 fun calcularPropina( subtotal: Double = 0.0, modifier: Modifier = Modifier
     .fillMaxSize()
@@ -74,12 +177,13 @@ fun calcularPropina( subtotal: Double = 0.0, modifier: Modifier = Modifier
 
     }
 }
-
+*/
 
 @Preview(showBackground = true)
 @Composable
 fun previsualizacion() {
-    calcularPropina()
+//    calcularPropina()
+    calcularPropina2()
 }
 /*
 fun editarCampoPropina(modifier: Modifier){
